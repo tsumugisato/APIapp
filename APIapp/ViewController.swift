3d8149c52f71aba52c1a320e3682e0c34f48c6c9
satoutsumugi <tsumugisato0@gmail.com>
2021-11-07 05:23:05 +0900

    Initial Commit

---
+++ APIapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIapp
+//
+//  Created by 佐藤紬 on 2021/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
